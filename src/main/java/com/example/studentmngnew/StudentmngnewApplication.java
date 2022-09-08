9b0bda33d316413ae3250c6b5efc47a2c36cee16
107Neel <20IT071@charusat.edu.in>
2022-09-08 14:20:35 +0530

    Added Student Entity

---
+++ src/main/java/com/example/studentmngnew/StudentmngnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmngnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmngnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmngnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentmngnew/StudentmngnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmngnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmngnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
